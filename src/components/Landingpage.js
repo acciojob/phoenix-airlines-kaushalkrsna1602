becf57aa29755938838557415c5b59b0028989d3
kaushalkrsna1602 <kaushal.kr.kr@gmail.com>
2025-02-18 01:29:37 +0530

    c56

--- src/components/Landingpage.js
+++ src/components/Landingpage.js
@@ -2,19 +2,19 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 
 function Landingpage() {
-    let navigate = useNavigate();
+  let navigate = useNavigate();
 
-    const nextpage = () => {
-        navigate("/flight-search");
-        console.log("hi");
-    }
+  const nextpage = () => {
+    navigate("/flight-search");
+    console.log("hi");
+  }
 
-    return (
-        <div>
-            <h1>Welcome To Flight Booking App</h1>
-            <button onClick={nextpage}>Search Flights Here</button>
-        </div>
-    )
+  return (
+    <div>
+      <h1>Welcome To Flight Booking App</h1>
+      <button onClick={nextpage}>Search Flights Here</button>
+    </div>
+  );
 }
 
 export default Landingpage;
--- src/components/Searchpage.js
+++ src/components/Searchpage.js
@@ -4,44 +4,43 @@ import { useDispatch, useSelector } from "react-redux";
 import { storeJourneydata } from "../redux/actions/journeyDetailsActions";
 
 function Searchpage() {
-    let dispatch = useDispatch();
-    let reduxdata = useSelector((state) => { return state });
-    
-    const [radio, setRadio] = useState("");
-    const [from, setFrom] = useState("");
-    const [to, setTo] = useState("");
-    const [date, setDate] = useState("");
-    const [search, setSearch] = useState(false);
+  let dispatch = useDispatch();
+  let reduxdata = useSelector((state) => state);
 
-    let navigate = useNavigate();
-    const searchclicked = () => {
-        if (radio && date && from && to) {
-            console.log(radio, from, to, date);
-            dispatch(storeJourneydata({ radio, from, to, date }));
+  const [radio, setRadio] = useState("");
+  const [from, setFrom] = useState("");
+  const [to, setTo] = useState("");
+  const [date, setDate] = useState("");
+  const [search, setSearch] = useState(false);
 
-            setSearch(true);
-        }
-        else {
-            alert("All the fields are required");
-            setSearch(false);
-        }
+  let navigate = useNavigate();
+  const searchclicked = () => {
+    if (radio && date && from && to) {
+      console.log(radio, from, to, date);
+      dispatch(storeJourneydata({ radio, from, to, date }));
+      setSearch(true);
+    } else {
+      alert("All the fields are required");
+      setSearch(false);
     }
+  }
 
-    return (
-        <div>
-            {!search && <div>
-                <input type="radio" name="radiobtn" id="oneway" value="One Way" onChange={(e) => { setRadio(e.target.value) }} />
-                <label htmlFor="oneway">One Way Trip</label>
-                <input type="radio" name="radiobtn" id="round" value="Round Trip" onChange={(e) => { setRadio(e.target.value) }} />
-                <label htmlFor="round">Round Trip</label> <br />
+  return (
+    <div>
+      {!search && <div>
+        <input type="radio" name="radiobtn" id="oneway" value="One Way" onChange={(e) => { setRadio(e.target.value) }} />
+        <label htmlFor="oneway">One Way Trip</label>
+        <input type="radio" name="radiobtn" id="round" value="Round Trip" onChange={(e) => { setRadio(e.target.value) }} />
+        <label htmlFor="round">Round Trip</label> <br />
 
-                <input type="text" placeholder="Source City" value={from} onChange={(e) => { setFrom(e.target.value) }} /> <br />
-                <input type="text" placeholder="Destination City" value={to} onChange={(e) => { setTo(e.target.value) }} /> <br />
-                <input type="date" value={date} onChange={(e) => { setDate(e.target.value) }} /> <br />
+        <input type="text" placeholder="Source City" value={from} onChange={(e) => { setFrom(e.target.value) }} /> <br />
+        <input type="text" placeholder="Destination City" value={to} onChange={(e) => { setTo(e.target.value) }} /> <br />
+        <input type="date" value={date} onChange={(e) => { setDate(e.target.value) }} /> <br />
 
-                <button onClick={searchclicked}>Search Flight</button>
-            </div>}
-            {search && <div>
+        <button onClick={searchclicked}>Search Flight</button>
+      </div>}
+      {search && <div>
+
                 <button onClick={() => { setSearch(false) }}>Search Flight</button>
                 <ul>
                     <li>Air India AI-275 Departure: 12:00 Journey Time: 1 Hr 25 Min Rs.3,600 <button onClick={() => { dispatch(storeJourneydata({ ...reduxdata, flightname: "Air India AI-275" })); navigate("/flight-booking") }}>Book Now</button></li>
